ef6b0917cb7ad13878e1fcfb91778c76b4c31618
MoureDev <braismoure@mouredev.com>
2024-04-19 13:25:26 +0200

    Fotos speakers

--- web/views/partners.py
+++ web/views/partners.py
@@ -13,6 +13,11 @@ def partners() -> rx.Component:
         terminal_text(quoted_text="Patrocinado", end_text=" por"),
         rx.flex(
             partner(
+                "/partners/platzi.png",
+                "https://platzi.com/BraisconfMX",
+                "Estudia programación"
+            ),
+            partner(
                 "/partners/raiola.png",
                 "https://mouredev.com/raiola",
                 "20% en hosting"
@@ -31,23 +36,23 @@ def partners() -> rx.Component:
             wrap="wrap"
         ),
         rx.spacer(),
-        rx.text("¿Quieres patrocinar el evento? ¡NO TENGO PALABRAS!"),
-        rx.text(
-            "Puedes escribirme a ",
-            rx.link(
-                "braismoure@mouredev.com",
-                href="mailto:braismoure@mouredev.com",
-                is_external=True
-            ),
-            " o contactarme a través de mis redes sociales como ",
-            rx.link(
-                "@mouredev",
-                href=constants.MOUREDEV_URL,
-                is_external=True
-            ),
-            "."
-        ),
-        print_text("El patrocinio servirá para pagar los participantes, realizar sorteos y cubrir los gastos de la organización. También se realizarán menciones y aparecerás destacado en todas las comunicaciones relacionadas con el evento."),
+        # rx.text("¿Quieres patrocinar el evento? ¡NO TENGO PALABRAS!"),
+        # rx.text(
+        #     "Puedes escribirme a ",
+        #     rx.link(
+        #         "braismoure@mouredev.com",
+        #         href="mailto:braismoure@mouredev.com",
+        #         is_external=True
+        #     ),
+        #     " o contactarme a través de mis redes sociales como ",
+        #     rx.link(
+        #         "@mouredev",
+        #         href=constants.MOUREDEV_URL,
+        #         is_external=True
+        #     ),
+        #     "."
+        # ),
+        # print_text("El patrocinio servirá para pagar los participantes, realizar sorteos y cubrir los gastos de la organización. También se realizarán menciones y aparecerás destacado en todas las comunicaciones relacionadas con el evento."),
         button(
             constants.COFFEE_URL,
             "Colabora con un café",
