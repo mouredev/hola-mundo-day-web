74762af0d223f7c6514d44346c379039714d320e
Brais Moure <mouredev@gmail.com>
2024-05-23 08:24:45 +0200

    Vídeo charlas

--- web/views/speakers.py
+++ web/views/speakers.py
@@ -18,49 +18,57 @@ def speakers() -> rx.Component:
             "Github es la red social de los programadores y nuestro portfolio de trabajo. Es la ventana a nuestra experiencia técnica y las empresas le dan mucha importancia. Por esta razón, si somos desarrolladores de software, deberíamos preguntarnos... ¿Sé cómo presentarme en Github? En esta charla aprenderás tips y sugerencias para que tu profile de Github sea llamativo, además te contaré lo que me ha ayudado a mi a mejorar mi perfil.",
             "Pablo Marino",
             "https://www.linkedin.com/in/pablomarinotech",
-            "/speakers/pablo_marino.jpg"
+            "/speakers/pablo_marino.jpg",
+            "https://youtu.be/FKuwHUzAkQ4"
+
         ),
         _speaker(
             "Ser autodidacta en el mundo del software",
             "Hablaré sobre cómo aprender a ser autodidacta y contaré mi experiencia de cómo me cambió la vida. Cómo en el mundo del desarrollo necesitas estar siempre al día y cómo un desarrollador nunca para de aprender. Además, explicaré como bonus las ventajas de usar Jetpack Compose y SwiftUI.",
             "Kevin Morales",
             "https://www.kevinhomorales.com",
-            "/speakers/kevin_morales.jpg"
+            "/speakers/kevin_morales.jpg",
+            "https://youtu.be/PJy7zyPnXiM"
         ),
         _speaker(
             "Soft skills que todo programador debería tener",
             "En ocasiones, priorizamos el conocimiento técnico sobre el desarrollo de habilidades blandas. En esta charla exploraré la importancia de equilibrar ambos aspectos para el crecimiento profesional.",
             "Geraldhy Messu",
             "https://github.com/gerald-M14",
-            "/speakers/geraldhy_messu.jpg"
+            "/speakers/geraldhy_messu.jpg",
+            "https://youtu.be/IUf0L9ohYV4"
         ),
         _speaker(
             "Cómo entrar el mundo de freelance internacional: motivaciones, estrategias y retos",
             "El freelancing es una oportunidad genial para ganar dinero desde cualquier lugar del mundo, usando tus habilidades y destrezas. Tiene sentido considerar esta opción y entender sus ventajas y desventajas, sin importar el nivel. Como Upwork Top Rated Plus, quisiera compartir mis experiencias, métodos y estrategias.",
             "Dmitry Zhukov",
             "https://www.linkedin.com/in/dmitryjima",
-            "/speakers/dmitry_zhukov.jpg"
+            "/speakers/dmitry_zhukov.jpg",
+            "https://youtu.be/MSFnVxafA7E"
         ),
         _speaker(
             "¡No dejes para mañana tu código de hoy! Cómo vencer la procrastinación en programación",
             "Como alguien que ha superado la procrastinación en programación, estoy aquí para compartir cómo logré vencer este obstáculo. Aprendí a establecer metas claras, dividir proyectos en tareas manejables y eliminar distracciones. Con determinación y autodisciplina, logré mejorar mi productividad y calidad de trabajo. Ahora quiero ayudarte a hacer lo mismo. Juntos, podemos superar la procrastinación y alcanzar nuestro máximo potencial como desarrolladores.",
             "José Ángel Polanco",
             "https://www.instagram.com/joseangelpolanco77",
-            "/speakers/jose_angel.jpg"
+            "/speakers/jose_angel.jpg",
+            "https://youtu.be/0qA7wUzMwqg"
         ),
         _speaker(
             "Desbloquea tu creatividad: Prompt engineering con Gemini AI y ChatGPT",
             "¡Despierta tu lado creativo! Aprende a usar Prompt Engineering con Gemini AI y ChatGPT para generar textos increíbles y soluciones creativas para tus proyectos, desde recomendaciones en Markdown hasta estructuras de tú código optimizadas.",
             "Juliana Castillo",
             "https://www.linkedin.com/in/julianacastilloaraujo",
-            "/speakers/juliana_castillo.jpg"
+            "/speakers/juliana_castillo.jpg",
+            "https://youtu.be/REhrWtTQsuo"
         ),
         _speaker(
             "UX Design",
             "Explora las claves del diseño de experiencia de usuario para crear productos que no solo satisfagan, sino que deleiten. Descubre cómo la empatía y la innovación se unen para transformar la interacción usuario-producto.",
             "Angela Martínez",
             "https://www.linkedin.com/in/amfprogramacion/",
-            "/speakers/angela_martinez.jpg"
+            "/speakers/angela_martinez.jpg",
+            "https://youtu.be/I7EQtk1Y1q0"
         ),
 
         terminal_text(quoted_text="Talleres"),
@@ -69,21 +77,24 @@ def speakers() -> rx.Component:
             "Taller práctico donde te explicarán paso a paso cómo configurar tu entorno de desarrollo para contribuir a proyectos de código abierto, repasando comandos útiles, configuración de SSH, personalización del editor (Visual Studio Code), etc. Como ejemplo práctico, configuraremos un repo desde cero para contribuir al roadmap de la comunidad.",
             "Jamer José",
             "https://jamerrq.deno.dev",
-            "/speakers/jamer_jose.jpg"
+            "/speakers/jamer_jose.jpg",
+            "https://youtu.be/1YDgrgX-70c"
         ),
         _speaker(
             "Crea tu propio bot de Telegram con Python",
             "Descubre cómo crear un bot de Telegram utilizando la biblioteca Telebot de Python. Explorarás la interacción con la API de Telegram y aprenderás a configurar respuestas automáticas a los mensajes.",
             "Karoll Escalante",
             "https://www.linkedin.com/in/karollescalanteg",
-            "/speakers/karoll_escalante.jpg"
+            "/speakers/karoll_escalante.jpg",
+            "https://youtu.be/wt5IVr1eibE"
         ),
         _speaker(
             "Desmitificando el Testing: Aprende a probar como un profesional",
             "¿Te apasiona el mundo del software y quieres aprender a probar como un profesional? En este taller práctico te guiaré a través de los fundamentos del testing de software, desde los conceptos básicos hasta las técnicas más avanzadas.",
             "María Morán",
             "https://twitter.com/_mariamoraan",
-            "/speakers/maria_moran.jpg"
+            "/speakers/maria_moran.jpg",
+            "https://youtu.be/rP31-GNMgok"
         ),
         text(
             "NO se han admitido propuestas de personas con experiencia como ponentes.",
@@ -115,7 +126,7 @@ def speakers() -> rx.Component:
     )
 
 
-def _speaker(title: str, body: str, author: str, url: str, avatar: str) -> rx.Component:
+def _speaker(title: str, body: str, author: str, url: str, avatar: str, video: str) -> rx.Component:
     return rx.flex(
         rx.image(
             src=avatar,
@@ -133,6 +144,12 @@ def _speaker(title: str, body: str, author: str, url: str, avatar: str) -> rx.Co
                 f"- {author}",
                 href=url,
                 is_external=True
+            ),
+            button(
+                video,
+                "Ver vídeo",
+                "youtube",
+                True
             ),
             rx.divider()
         ),
